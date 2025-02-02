d0027461be2ba22c9e530c5e34caf3b15620f821
Samuel Renaud <2392263@csfoy.ca>
2025-02-02 14:12:59 -0500

    added wizard tool

---
+++ internal/generator/wizard.go
@@ -0,0 +1,103 @@
+package generator
+
+import (
+	"fmt"
+	"passgengo/internal/hashing"
+	"passgengo/internal/logging"
+	"strconv"
+	"strings"
+)
+
+func GetGeneratorFromWizard() (Generator, error) {
+	builder := NewGeneratorBuilder()
+
+	length, err := getUserIntInput(
+		fmt.Sprintf("Please enter the password length (min: %d, max: %d): ", MinPassLen, MaxPassLen),
+		func(i int) bool { return i >= MinPassLen && i <= MaxPassLen },
+	)
+	if err != nil {
+		return nil, err
+	}
+	builder.Length(length)
+
+	noUpper, err := getYesNoInput("Do you wish to disable upper case characters ?")
+	if err != nil {
+		return nil, err
+	}
+	if noUpper {
+		builder.NoUpperChars()
+	}
+
+	noSpecial, err := getYesNoInput("Do you wish to disable special characters ?")
+	if err != nil {
+		return nil, err
+	}
+	if noSpecial {
+		builder.NoSpecialChars()
+	}
+
+	hasHashing, err := getYesNoInput("Do you wish to hash your password ?")
+	if err != nil {
+		return nil, err
+	}
+	if hasHashing {
+		hashingMethods := hashing.GetHashingMethodsString()
+		hashingMethod, err := getUserIntInput(
+			fmt.Sprintf("Choose a hashing method from the list: \n%s\n", hashingMethods),
+			func(i int) bool { return i >= 0 && i < len(hashing.HashMethods) },
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		builder.HashingMethod(hashingMethod)
+	}
+
+	return builder.Build(), nil
+}
+
+func getYesNoInput(message string) (bool, error) {
+	message = message + "(y/n): "
+	logging.Input(message)
+
+	var input string
+	isNotValid := true
+	for isNotValid {
+		if _, err := fmt.Scan(&input); err != nil {
+			return false, err
+		}
+
+		input = strings.ToLower(input)
+		isNotValid = input != "y" && input != "n"
+
+		if isNotValid {
+			logging.Warning("The input is invalid")
+			logging.Input(message)
+		}
+	}
+
+	return input == "y", nil
+}
+
+func getUserIntInput(message string, condition func(int) bool) (int, error) {
+	logging.Input(message)
+
+	var input string
+	isNotValid := true
+	num := -1
+	for isNotValid {
+		var err error
+		if _, err := fmt.Scan(&input); err != nil {
+			return -1, err
+		}
+
+		num, err = strconv.Atoi(input)
+		isNotValid = err != nil || !condition(num)
+
+		if isNotValid {
+			logging.Warning("The input is not valid, try again")
+			logging.Input(message)
+		}
+	}
+	return num, nil
+}
